bd21ae62336512c51f04277747f8c253896bf3b8
madrodgerflynn <madrodgerflynn@gmail.com>
2022-09-29 22:17:38 -0600

    working on server.js

--- controller/userController.js
+++ controller/userController.js
@@ -100,7 +100,7 @@ module.exports = {
     )
       .then((user) =>
         !user
-          ? res.status(404).json({ message: "No user with this id!" })
+          ? res.status(404).json({ message: "No thought!" })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
