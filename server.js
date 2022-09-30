385835c9db33738666b853858dac257e974ce68f
madrodgerflynn <madrodgerflynn@gmail.com>
2022-09-29 23:28:25 -0600

    Fixing server routes

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const db = require("./config/connection");
 const routes = require("./routes");
 
 const cwd = process.cwd();
+
+const PORT = 3001;
+const app = express();
+
+const activity = cwd.includes("01-Activities")
+  ? cwd.split("/01-Activities/")[1]
+  : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
